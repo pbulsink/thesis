b423bb4cf363ca2140141b365d896f6e2eb4c51f
Phil OSx <philip.bulsink@uottawa.ca>
2014-08-22 16:53:11 -0400

    First comments from Darrin

--- proper/buildthesis.sh
+++ proper/buildthesis.sh
@@ -8,9 +8,12 @@ makeglossaries thesis
 pdflatex thesis
 pdflatex thesis
 echo “Thesis Built”
+rm checkcites.log 
+checkcites thesis.aux 2>&1 | tee checkcites.log
 #dvipdfm thesis.dvi
 ENDTIME=$(date +%s)
 echo PDF Built and Saved in $(($ENDTIME - $STARTTIME)) seconds at $(date)
 open thesis.pdf
+tail -n checkcites.log
 exit
 
